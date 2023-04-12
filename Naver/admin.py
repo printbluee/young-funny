3814234b655c7b1d8b28f9c88bfcb1c426af9936
with_blue <yhdosa0622@gmail.com>
2023-04-12 17:57:47 +0900

    모델 수정 완료

--- Naver/admin.py
+++ Naver/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Naver_person
 
 class Naver_personAdmin(admin.ModelAdmin):
-    list_display = ['uuid', 'title', 'question', 'question_ID', 'creat_at', 'adoption_status']
+    list_display = ['title', 'question', 'question_ID', 'creat_at', 'adoption_status', 'uuid']
     # uuid / 질문 제목 / 질문자 아이디 / 작성일 / 채택 여부
     search_fields = ('title', 'question', 'question_ID', 'answer_ID' )
 admin.site.register(Naver_person,Naver_personAdmin)
